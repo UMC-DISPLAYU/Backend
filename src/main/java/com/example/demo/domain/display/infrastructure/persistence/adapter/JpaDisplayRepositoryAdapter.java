03795d4929e14242134c9edf312af0fd7dfa1b7b
Lidohyeon <ehgus3193@naver.com>
2026-07-09 20:15:37 +0900

    feat: infrastructure 구현 ( adpater, jparepository)

---
+++ src/main/java/com/example/demo/domain/display/infrastructure/persistence/adapter/JpaDisplayRepositoryAdapter.java
@@ -0,0 +1,38 @@
+package com.example.demo.domain.display.infrastructure.persistence.adapter;
+
+import com.example.demo.domain.display.domain.aggregate.Display;
+import com.example.demo.domain.display.domain.repository.DisplayRepository;
+import com.example.demo.domain.display.infrastructure.persistence.SpringDataDisplayJpaRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JpaDisplayRepositoryAdapter implements DisplayRepository {
+
+  private final SpringDataDisplayJpaRepository jpaRepository;
+
+  public JpaDisplayRepositoryAdapter(SpringDataDisplayJpaRepository jpaRepository) {
+    this.jpaRepository = jpaRepository;
+  }
+
+  @Override
+  public Optional<Display> findById(Long displayId) {
+    return jpaRepository.findById(displayId);
+  }
+
+  @Override
+  public List<Display> findAll() {
+    return jpaRepository.findAll();
+  }
+
+  @Override
+  public Display save(Display display) {
+    return jpaRepository.save(display);
+  }
+
+  @Override
+  public void delete(Display display) {
+    jpaRepository.delete(display);
+  }
+}
