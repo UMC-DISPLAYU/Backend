28f4181b7241069bb01313fea5154b85bbf0445a
Lidohyeon <ehgus3193@naver.com>
2026-07-10 15:09:31 +0900

    refactor: 더미데이터 insert문 수정

--- src/main/resources/db/migration/V3__create_display_field_table.sql
+++ src/main/resources/db/migration/V3__create_display_field_table.sql
@@ -4,9 +4,13 @@ CREATE TABLE `DisplayField` (
                                 `sortOrder` INT NOT NULL,
                                 `displayId` BIGINT NOT NULL,
                                 CONSTRAINT `PK_DISPLAYFIELD` PRIMARY KEY (`displayFieldId`),
-                                CONSTRAINT `UK_DISPLAYFIELD_DISPLAY_FIELD` UNIQUE (`displayId`, `field`)
+                                CONSTRAINT `UK_DISPLAYFIELD_DISPLAY_FIELD` UNIQUE (`displayId`, `field`),
+                                CONSTRAINT `FK_DISPLAYFIELD_DISPLAY` FOREIGN KEY (`displayId`) REFERENCES `Display` (`displayId`)
 );
 
 INSERT INTO `DisplayField` (`field`, `sortOrder`, `displayId`)
 SELECT `displayField`, 0, `displayId`
 FROM `Display`;
+
+ALTER TABLE `Display`
+    DROP COLUMN `displayField`;
--- src/main/resources/db/migration/V4__drop_display_display_field_column.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `Display`
-    DROP COLUMN `displayField`;
