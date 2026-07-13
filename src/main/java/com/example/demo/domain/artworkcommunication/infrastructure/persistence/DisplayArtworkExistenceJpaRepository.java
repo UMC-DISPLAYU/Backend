dbceb48daf5f3b1a49873a2b723eec3ff66c9f61
김민지 <meeji12345@naver.com>
2026-07-13 11:41:15 +0900

    style: JPA repository 포맷 적용

--- src/main/java/com/example/demo/domain/artworkcommunication/infrastructure/persistence/DisplayArtworkExistenceJpaRepository.java
+++ src/main/java/com/example/demo/domain/artworkcommunication/infrastructure/persistence/DisplayArtworkExistenceJpaRepository.java
@@ -2,4 +2,5 @@ package com.example.demo.domain.artworkcommunication.infrastructure.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DisplayArtworkExistenceJpaRepository extends JpaRepository<DisplayArtworkReferenceJpaEntity, Long> {}
+public interface DisplayArtworkExistenceJpaRepository
+    extends JpaRepository<DisplayArtworkReferenceJpaEntity, Long> {}
