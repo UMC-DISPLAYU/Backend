8bd1e8d73741364964ce440cc3784fc9f4be834c
최건희 <gunhee@MacBook-Pro.local>
2026-07-14 15:50:21 +0900

    fix: 라운지 좋아요/스크랩 응답 필드명 수정

--- src/main/java/com/example/demo/domain/lounge/presentation/response/LoungePostScrapResponse.java
+++ src/main/java/com/example/demo/domain/lounge/presentation/response/LoungePostScrapResponse.java
@@ -1,3 +1,3 @@
 package com.example.demo.domain.lounge.presentation.response;
 
-public record LoungePostScrapResponse(Long loungePostId, boolean scrapped, long scrapCount) {}
+public record LoungePostScrapResponse(Long loungePostId, boolean isScrapped, long scrapCount) {}
