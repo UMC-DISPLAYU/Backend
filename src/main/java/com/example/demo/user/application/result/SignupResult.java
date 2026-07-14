2f986f2a41861947491319ced9c058f4663bae25
wooarling <gripissmin1@gmail.com>
2026-07-14 15:57:45 +0900

    fix: remove duplicated user package

--- src/main/java/com/example/demo/user/application/result/SignupResult.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.user.application.result;
-
-import com.example.demo.user.domain.entity.User;
-
-public record SignupResult(User user, String accessToken, String refreshToken) {}
