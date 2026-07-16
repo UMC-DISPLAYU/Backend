6e42c46602de72efc030f435e22f06c5c9077491
Subin Kim <Sbunny2517@cau.ac.kr>
2026-07-17 00:19:48 +0900

    fix: 코드래빗 리뷰 반영 - 중복 메모 정리를 하드 삭제 대신 소프트 삭제로 변경
    유니크 제약 추가 전 중복 활성 메모를 DELETE로 물리 삭제하던 것을,
    프로젝트의 소프트 삭제 정책에 맞게 deletedAt을 채우는 UPDATE로 변경.
    로컬 DB 초기화 후 재적용 및 정상 동작 재검증 완료.

--- src/main/resources/db/migration/V20260716200000__add_memo_active_unique_constraint.sql
+++ src/main/resources/db/migration/V20260716200000__add_memo_active_unique_constraint.sql
@@ -1,18 +1,20 @@
-DELETE m1 FROM `Memo` m1
+UPDATE `Memo` m1
 INNER JOIN `Memo` m2
   ON m1.`archiveDisplayId` = m2.`archiveDisplayId`
   AND m1.`archiveDisplayId` IS NOT NULL
   AND m1.`deletedAt` IS NULL
   AND m2.`deletedAt` IS NULL
-  AND m1.`memoId` < m2.`memoId`;
+  AND m1.`memoId` < m2.`memoId`
+SET m1.`deletedAt` = NOW(6);
 
-DELETE m1 FROM `Memo` m1
+UPDATE `Memo` m1
 INNER JOIN `Memo` m2
   ON m1.`archiveWorkId` = m2.`archiveWorkId`
   AND m1.`archiveWorkId` IS NOT NULL
   AND m1.`deletedAt` IS NULL
   AND m2.`deletedAt` IS NULL
-  AND m1.`memoId` < m2.`memoId`;
+  AND m1.`memoId` < m2.`memoId`
+SET m1.`deletedAt` = NOW(6);
 
 ALTER TABLE `Memo`
   ADD COLUMN `activeArchiveDisplayId` BIGINT
