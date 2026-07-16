858ecbd4d58e83da143de0756f9b37318df84f5b
김민지 <meeji12345@naver.com>
2026-07-16 19:42:38 +0900

    fix: migration 충돌 방지

--- src/main/resources/db/migration/V20260714000300__add_user_id_to_artwork_feeling_reply.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE `ArtworkFeelingReply`
-    ADD COLUMN `userId` BIGINT NULL AFTER `feelingId`;
-
-UPDATE `ArtworkFeelingReply`
-SET `userId` = 1
-WHERE `userId` IS NULL;
-
-ALTER TABLE `ArtworkFeelingReply`
-    MODIFY COLUMN `userId` BIGINT NOT NULL;
