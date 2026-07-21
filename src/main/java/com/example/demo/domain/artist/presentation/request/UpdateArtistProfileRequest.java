2e9c714d92209cb35bfb6b8728483d4847fbaad4
wooarling <gripissmin1@gmail.com>
2026-07-21 22:12:03 +0900

    refactor : swagger 예시 수정

--- src/main/java/com/example/demo/domain/artist/presentation/request/UpdateArtistProfileRequest.java
+++ src/main/java/com/example/demo/domain/artist/presentation/request/UpdateArtistProfileRequest.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 public record UpdateArtistProfileRequest(
     @Schema(example = "https://cdn.example.com/images/user/profile.jpg") String profileImageUrl,
-    @Schema(example = "sangjun24") String nickname,
+    @Schema(example = "maya") String nickname,
     @Schema(example = "시각과 공간의 관계를 탐구하는 작가입니다.") String introduction,
     @Schema(example = "[\"DESIGN\", \"VIDEO\"]") List<ActivityCategory> fields,
-    @Schema(example = "https://portfolio.sangjun.com") String externalLink,
-    @Schema(example = "한양대학교") String univName) {}
+    @Schema(example = "https://portfolio.maya.com") String externalLink,
+    @Schema(example = "덕성여자대학교") String univName) {}
