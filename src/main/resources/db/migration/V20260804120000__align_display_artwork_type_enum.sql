0d8d0aa474c053862ffe898bc269112919fad19c
Lidohyeon <ehgus3193@naver.com>
2026-08-04 17:59:26 +0900

    chore: 마이그레이션 db 안정성 추가

--- src/main/resources/db/migration/V20260804120000__align_display_artwork_type_enum.sql
+++ src/main/resources/db/migration/V20260804120000__align_display_artwork_type_enum.sql
@@ -1,3 +1,21 @@
+ALTER TABLE DisplayArtwork
+    MODIFY COLUMN type enum (
+        'PAINTING',
+        'DESIGN',
+        'PHOTOGRAPHY',
+        'ARCHITECTURE',
+        'VIDEO',
+        'MEDIA',
+        'CRAFTS',
+        'CRAFT',
+        'SCULPTURE',
+        'FASHION',
+        'ILLUSTRATION',
+        'COMPLEX',
+        'OTHERS',
+        'ETC'
+    ) NOT NULL COMMENT 'PAINTING, DESIGN, PHOTOGRAPHY, ARCHITECTURE, MEDIA, CRAFT, SCULPTURE, FASHION, COMPLEX, ETC';
+
 UPDATE DisplayArtwork
 SET type = 'MEDIA'
 WHERE type = 'VIDEO';
