1d27f9948bbf63d525aeeabcff4454771553739d
최건희 <128896847+chlrjsgml@users.noreply.github.com>
2026-08-04 18:13:05 +0900

    fix: 라운지 게시글 제목 및 내용 글자 수 제한 변경

--- src/main/java/com/example/demo/domain/lounge/presentation/request/LoungePostRequest.java
+++ src/main/java/com/example/demo/domain/lounge/presentation/request/LoungePostRequest.java
@@ -8,9 +8,9 @@ import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record LoungePostRequest(
-    @NotBlank @Size(max = 50) String title,
+    @NotBlank @Size(max = 100) String title,
     @NotNull @Size(max = 5) List<@NotBlank @Size(max = 2048) String> postImageUrls,
-    @NotBlank @Size(max = 1000) String content,
+    @NotBlank @Size(max = 1500) String content,
     @NotNull LoungePostCategory category) {
   public LoungePostContentCommand toCommand() {
     return new LoungePostContentCommand(title, postImageUrls, content, category);
