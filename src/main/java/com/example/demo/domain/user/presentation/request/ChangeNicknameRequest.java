64cb0a17a603f7b5c0e0abcfbb3db0ed90cafe82
wooarling <gripissmin1@gmail.com>
2026-08-08 16:26:36 +0900

    [DU-145] [Fix] 닉네임 정책 불일치 수정 및 학교 인증 에러 코드 추가 (#338)
    * fix: 닉네임 정책 수정
    
    * fix: 학교 인증 응답 에러 코드 수정

--- src/main/java/com/example/demo/domain/user/presentation/request/ChangeNicknameRequest.java
+++ src/main/java/com/example/demo/domain/user/presentation/request/ChangeNicknameRequest.java
@@ -3,5 +3,5 @@ package com.example.demo.domain.user.presentation.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ChangeNicknameRequest(
-    @Schema(description = "변경할 닉네임. 한글, 영문, 숫자만 사용하여 5~15자로 입력합니다.", example = "User02")
+    @Schema(description = "변경할 닉네임. 한글, 영문, 숫자만 사용하여 2~15자로 입력합니다.", example = "마야")
         String nickname) {}
